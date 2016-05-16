0b0a832371c96c07221e0d34ee0ad64a97b4a3f0
Fitch <francois-garcia@live.fr>
2016-05-16 14:21:26 +0200

    Premier commit

---
+++ echauffement/Chien.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace echauffement
+{
+    public class Chien
+    {
+        public string nomChien;
+        public int age;
+        public int pate;
+        public bool queue;
+
+        public Chien(string c_nomChat, int c_age, int c_pate, bool c_queue)
+        {
+            this.nomChien = c_nomChat;
+            this.age = c_age;
+            this.pate = c_pate;
+            this.queue = c_queue;
+        }
+    }
+}
---
+++ echauffement/Persan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace echauffement
+{
+    public class Persan : Chat
+    {
+        public Persan(string c_nomChat)
+            : base(c_nomChat)
+            
+        {
+            this.nomChat = c_nomChat;
+            this.pate = 4;
+            this.queue = false;
+        }
+  
+    }
+}
